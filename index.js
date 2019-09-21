a37c6f46da6df9fb8fc24be6e400a65222e6980b
searching-form-5732 <kevin.downs04@gmail.com>
2019-09-21 08:51:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('div.target');
+}
+
+function increaseRankBy(n){
+  var li = document.querySelectorAll('ul.ranked-list li');
+  for(let i = 0; i < li.length; i++){
+    li[i].innerHTML = parseInt(li[i].innerHTML) + n;
+  }
+}
+
+function deepestChild(){
+  var nodeList = document.getElementById('grand-node').querySelector('div');
+  var next = 0
+    while(nodeList !== undefined){
+      next = nodeList.querySelector('div')
+      nodeList = next.querySelector('div')
+      if(next.innerHTML === 'boo!'){
+        return next
+      }
+    }
+  return next
+}
